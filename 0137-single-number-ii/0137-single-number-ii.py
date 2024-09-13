feffc81ba9c1411572681814c2bc598a5eea5368
Roshni <66792774+CeraMapleheart@users.noreply.github.com>
2024-09-13 23:59:47 +0530

    Time: 29 ms (93.31%), Space: 13.9 MB (28.76%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        d={}
+        for i in nums:
+            if i not in d:
+                d[i] = 1
+            else:
+                if d[i] == 1:
+                    d[i] = 2
+                else:
+                    del d[i]
+        return list(d.keys())[0]
+
+        
